2f2f812a26947140a355bc4cbcd7e6665ea594ad
Gabriel <Gabbe.the.nord@gmail.com>
2021-04-08 16:52:34 +0200

    Test complete

--- mailChimpGN/src/test/java/stepDefinitions/StepDefinition.java
+++ mailChimpGN/src/test/java/stepDefinitions/StepDefinition.java
@@ -38,46 +38,57 @@ public class StepDefinition {
 		//Använder sig av både mailen och en nummerslumpare för att få dynamiska email och skriver sedan in på mailchimp.
 		@Given("I want to enter my {string}")
 		public void i_want_to_enter_my_(String mail) {
-			int numuse1 = numgen();
-		    WebElement emailBox = driver.findElement(By.id("email"));
-		    emailBox.sendKeys(numuse1 + mail);
-		    System.out.println(numuse1 + mail);
+		    if(mail.equals("blank")) {
+		    	
+		    }
+		    else {
+		    	WebElement emailBox = driver.findElement(By.id("email"));
+		    	int numuse1 = numgen();
+			    emailBox.sendKeys(numuse1 + mail);
+			    System.out.println(numuse1 + mail);
+		    }
 		}
 		//Använder sig av både användarnamn och en nummerslumpare för att få dynamiska användarnamn och skriver sedan in på mailchimp.
-		@And("I then need to enter my {string}")
-		public void i_then_need_to_enter_my_(String user) {
+		@And("I then enter my {string}")
+		public void i_then_enter_my_(String user) {
 			WebElement userBox = driver.findElement(By.id("new_username"));
-			int numuse2 = numgen();
-		    userBox.sendKeys(numuse2 + user);
-		    System.out.println(numuse2 + user);
+			if (user.equals("generiskanvandare")) {
+				userBox.sendKeys(user);
+				System.out.println(user);
+			}
+			else {
+				int numuse2 = numgen();
+			    userBox.sendKeys(numuse2 + user);
+			    System.out.println(numuse2 + user);
+			}
 		}
 		
 		//Skriver in ett giltigt lösenord
-		@Given("I also want to enter my {string}")
-		public void i_also_want_to_enter_my_(String pass) {
+		@And("I also enter my {string}")
+		public void i_also_enter_my_(String pass) {
 			WebElement passBox = driver.findElement(By.id("new_password"));
 		    passBox.sendKeys(pass);
 		}
 		
 		//klickar på Sign Up
-		@When("I am done entering my info i click Sign Up")
+		@When("I am done entering my info I click Sign Up")
 			public void i_am_dont_entering_my_info_i_click_sign_up() {
 			click(By.id("onetrust-accept-btn-handler"));
 			click(By.id("create-account"));
 			}
 
 		//verifierar att man kommer till rätt sida om testfallet går igenom
-		@Then("I get succes Lastly I verify with {string}")
-		public void lastly_i_verify_with_(String check) {
+		@Then("I decide if test should give error or not with succes and lastly I verify with {string}")
+		public void verify1(String check) {
 			WebElement doable = driver.findElement(By.xpath("//*[text()='" + check + "']"));
 			assertEquals(check,doable.getText());
 		}
 	
 		//verifierar att man får rätt felmeddelande
-		@Then("I get fail Lastly I verify with {string}")
-		public void i_get_fail_lastly_i_verify_with(String check) {
-			WebElement doable = driver.findElement(By.xpath("//*[text()='" + check + "']"));
-			assertEquals(check,doable.getText());
+		@Then("I decide if test should give error or not with fail and lastly I verify with {string}")
+		public void verify2(String check) {
+			WebElement doable = driver.findElement(By.xpath("//*[contains(text(),'" + check + "')]"));
+			assertEquals(true,doable.isDisplayed());
 		}
 
 
@@ -94,5 +105,3 @@ public class StepDefinition {
 			driver.findElement(by).click();
 		}
 }
-
-//given conext when actions then assertion/consequenses and repeat
--- mailChimpGN/src/test/resources/Features/accountCreation.feature
+++ mailChimpGN/src/test/resources/Features/accountCreation.feature
@@ -4,14 +4,17 @@ Feature: Account creation
   Scenario Outline: Mailchimp account
   
     Given I want to enter my "<email>"
-    And I then need to enter my "<username>" 
-    Given I also want to enter my "<password>"
-    When I am done entering my info i click Sign Up
-    Then I get <status> Lastly I verify with "<check>"
+    And I then enter my "<username>" 
+    And I also enter my "<password>"
+    When I am done entering my info I click Sign Up
+    Then I decide if test should give error or not with <status> and lastly I verify with <check>
     
 
     Examples: 
       | email  | username | password  | status | check |
-      | Gabbe@gmail.com |    Gabbegabbe | Super_sa:k3rt | succes | Check your email |
-      | Gabbe@gmail.com |  Gabbe123456789101112131415171819202122234252627282930313233343536373839404142434445464748495051525354 | Super_sa:k3rt | fail | Enter a value less than 100 characters long |
+      | Gabbe@gmail.com |  Gabbegabbe | Super_sa:k3rt | succes | "Check your email" |
+      | Gabbe@gmail.com |  Gabbe123456789101112131415171819202122234252627282930313233343536373839404142434445464748495051525354 | Super_sa:k3rt | fail |  " characters long"  |
+      | Gabbe@gmail.com | generiskanvandare |Super_sa:k3rt | fail | "Another user with this username already exists." |
+			| blank | Gabbegabbe | Super_sa:k3rt | fail | "Please enter a "|
+      
       
